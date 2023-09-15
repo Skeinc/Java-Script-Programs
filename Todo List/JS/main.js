e7236a0f2d611f3eb157371aeb355d29a25d8a40
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-09-15 08:28:04 +0300

    todo-list

--- Todo List/JS/main.js
+++ Todo List/JS/main.js
@@ -6,14 +6,16 @@ const addScreen = document.querySelector(".add-screen");
 const startButton = startScreen.children[3].children[0];
 const addButton = mainScreen.children[0].children[1];
 const cancelButton = addScreen.children[4].children[0];
-const completeButtons = document.querySelectorAll(".button-complete");
-const deleteButtons = document.querySelectorAll('.button-delete');
+const applyButton = addScreen.children[4].children[1];
 // Controllers
 const allControlller = mainScreen.children[1].children[0];
 const evenController = mainScreen.children[1].children[1];
 const oddController = mainScreen.children[1].children[2];
 const firstController = mainScreen.children[1].children[3];
 const lastController = mainScreen.children[1].children[4];
+// Inputs
+const nameInput = addScreen.children[2].children[0];
+const dateInput = addScreen.children[2].children[1];
 
 // User click to start button
 startButton.addEventListener('click', () => {
@@ -30,7 +32,7 @@ addButton.addEventListener('click', () => {
 });
 
 // User click to cancel button
-cancelButton.addEventListener('click', () => {
+cancelButton.addEventListener('click', turnToMainScreen = () => {
     addScreen.style.animation = "prevSlide 0.4s";
     setTimeout("addScreen.style.display = 'none';\nmainScreen.style.display = 'flex';", 400);
 });
@@ -62,6 +64,43 @@ function addActiveButton(button) {
     }
 
     return button.classList.add('active-button');
+}
+
+// Function for validate inputs data
+function validateData() {
+    try {
+        if(nameInput.value.length < 1 || nameInput.value.length > 30) {
+            throw new Error("Invalid length of name. Length must be from 1 to 30 characters.");
+        }
+        if(dateInput.value.length !== 10) {
+            throw new Error("Invalid format of date. Date must be like '31-12-2023'");
+        }
+        let date = dateInput.value.split("");
+        if(date[2] !== "-" || date[5] !== "-") {
+            throw new Error("Invalid format of date. Date must be like '31-12-2023'");
+        }
+        if(Number(date[0] + date[1]) < 1 || Number(date[0] + date[1]) > 31) {
+            throw new Error("Invalid format of date. Date must be like '31-12-2023'. Value of day must be from 1 to 31");
+        }
+        if(Number(date[3] + date[4]) < 1 || Number(date[3] + date[4]) > 12) {
+            throw new Error("Invalid format of date. Date must be like '31-12-2023'. Value of month must be from 1 to 12");
+        }
+        if(Number(date[6] + date[7] + date[8] + date[9]) < 1000 || Number(date[6] + date[7] + date[8] + date[9]) > 9999) {
+            throw new Error("Invalid format of date. Date must be like '31-12-2023'. Value of year must be from 1000 to 9999");
+        }
+        // If we haven't errors return true
+        return true;
+    }
+    catch(Error) {
+        addScreen.children[3].children[0].innerHTML = Error;
+        // If we have error return false
+        return false;
+    }
+}
+
+// Function "sleep"
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // User click to "All Controller"
@@ -109,7 +148,7 @@ oddController.addEventListener('click', () => {
 });
 
 // User click to "First Controller"
-firstController.addEventListener('click', () => {
+firstController.addEventListener('click', async () => {
     // Normalize to-do list
     normalizeList();
     // Add active button
@@ -124,12 +163,16 @@ firstController.addEventListener('click', () => {
     catch(error) {
         return alert(error);
     }
+    // Get delete animation to element
+    todoItems[0].style.animation = "deleteItem 0.4s";
+    // "sleep" code on 0.4s
+    await sleep(400);
     // Delete first element from to-do list
     return todoItems[0].remove();
 });
 
 // User click to "Last Controller"
-lastController.addEventListener('click', () => {
+lastController.addEventListener('click', async () => {
     // Normalize to-do list
     normalizeList();
     // Add active button
@@ -144,24 +187,114 @@ lastController.addEventListener('click', () => {
     catch(error) {
         return alert(error);
     }
+    // Get delete animation to element
+    todoItems[todoItems.length-1].style.animation = "deleteItem 0.4s";
+    // "sleep" code on 0.4s
+    await sleep(400);
     // Delete last element from to-do list
     return todoItems[todoItems.length-1].remove();
 });
 
-// User click to delete button
-for(let i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', () => {
-        return deleteButtons[i].parentElement.parentElement.remove();
-    });
-}
+// User click to delete button or user click to complete button on todo-list
+mainScreen.children[2].children[0].addEventListener('click', async (element) => {
+    if(element.target.classList.contains("button-delete")) {
+        // Get delete animation to element
+        element.target.parentElement.parentElement.style.animation = "deleteItem 0.4s";
+        // "sleep" code on 0.4s
+        await sleep(400);
+        // Delete item
+        return element.target.parentElement.parentElement.remove();
+    }
+    if(element.target.classList.contains("button-complete")) {
+        // Create item
+        const completedItem = ` <div class="completed-item">
+                                    <div class="todo-item-button">
+                                        <button type="submit" class="button-return">↻</button>
+                                    </div>
 
-// User click to complete button
-for(let i = 0; i < completeButtons.length; i++) {
-    completeButtons[i].addEventListener('click', () => {
-        const completedItem = completeButtons[i].parentElement.parentElement.cloneNode(true);
-        const todoList = document.querySelector(".todo-list");
-        completeButtons[i].parentElement.parentElement.remove();
-        todoList.append(completedItem);
-        console.log(completedItem);
-    });
-}
+                                    <div class="todo-item-information">
+                                        <span class="todo-item-name">${element.target.parentElement.parentElement.children[1].children[0].innerHTML}</span>
+                                        <span class="todo-item-date">${element.target.parentElement.parentElement.children[1].children[1].innerHTML}</span>
+                                    </div>
+
+                                    <div class="todo-item-button">
+                                        <button type="submit" class="button-delete">×</button>
+                                    </div>
+                                </div>`;
+        // Get complete animation to element
+        element.target.parentElement.parentElement.style.animation = "completeItem 0.4s";
+        // "sleep" code on 0.4s
+        await sleep(400);
+        // Delete this item from todo list
+        element.target.parentElement.parentElement.remove();
+        // Return completed item to completed items
+        return mainScreen.children[2].children[1].innerHTML += completedItem;
+    }
+});
+
+// User click to delete button or return button on completed items
+mainScreen.children[2].children[1].addEventListener('click', async (element) => {
+    if(element.target.classList.contains("button-delete")) {
+        // Get delete animation to element
+        element.target.parentElement.parentElement.style.animation = "deleteItem 0.4s";
+        // "sleep" code on 0.4s
+        await sleep(400);
+        // Delete item
+        return element.target.parentElement.parentElement.remove();
+    }
+    if(element.target.classList.contains("button-return")) {
+        // Create item
+        const todoItem = ` <div class="todo-item">
+                                    <div class="todo-item-button">
+                                        <button type="submit" class="button-complete">&#10004;</button>
+                                    </div>
+
+                                    <div class="todo-item-information">
+                                        <span class="todo-item-name">${element.target.parentElement.parentElement.children[1].children[0].innerHTML}</span>
+                                        <span class="todo-item-date">${element.target.parentElement.parentElement.children[1].children[1].innerHTML}</span>
+                                    </div>
+
+                                    <div class="todo-item-button">
+                                        <button type="submit" class="button-delete">×</button>
+                                    </div>
+                                </div>`;
+        // Get complete animation to element
+        element.target.parentElement.parentElement.style.animation = "completeItem 0.4s";
+        // "sleep" code on 0.4s
+        await sleep(400);
+        // Delete this item from todo list
+        element.target.parentElement.parentElement.remove();
+        // Return completed item to completed items
+        return mainScreen.children[2].children[0].innerHTML += todoItem;
+    }
+});
+
+// User click to apply button
+applyButton.addEventListener('click', () => {
+    validateData();
+    // If we haven't errors
+    if(validateData()) {
+        // Create new item with name and date from inputs
+        const newItem = `<div class="todo-item">
+                            <div class="todo-item-button">
+                                <button type="submit" class="button-complete">&#10004;</button>
+                            </div>
+
+                            <div class="todo-item-information">
+                                <span class="todo-item-name">${nameInput.value}</span>
+                                <span class="todo-item-date">${dateInput.value}</span>
+                            </div>
+
+                            <div class="todo-item-button">
+                                <button type="submit" class="button-delete">×</button>
+                            </div>
+                        </div>`;
+        // Clear values of inputs
+        nameInput.value = "";
+        dateInput.value = "";
+        // Turn to main screen
+        turnToMainScreen();
+        // Return new item to todo list
+        return mainScreen.children[2].children[0].innerHTML += newItem;
+    }
+});
